1c4caa2e2c618dc735b9f266238be3997107f058
Justin McAdams <mcadamsrails@gmail.com>
2013-04-23 19:22:54 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sunrise::Application.config.secret_token = '8d2bb8b75311504b3e3b76c7a7e98bf8890dc2525e48239ededfdfd4268809368d31ed9de5c908a8f70164cbc7fa89fa39417cd78fd9737dc669c89c9055f5be'
